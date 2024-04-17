5149e3b499a21ee7e66e8e9899f6838c98371e8e
Aiden <minwoo3180@gmail.com>
2024-04-17 19:50:07 -0400

    pxk plugin is prepared, ready to be merged

--- pxk/plugin.py
+++ pxk/plugin.py
@@ -1,4 +1,4 @@
-from .Constants import NAMESPACE_NAME
+from .Constants import NAMESPACE_NAME, CUSTOM_IMAGE_NAME
 from .k8_client import generate_k8_pods, delete_k8_deployment
 import logging
 import pytest
@@ -14,31 +14,53 @@ import socket
 from time import sleep
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--namespace", action="store", default=NAMESPACE_NAME, help="Defines the namespace of pods"
+    )
+    parser.addoption(
+        "--custom_image", action="store", default=CUSTOM_IMAGE_NAME, help="Defines the name of the custom image"
+    )
+
+
 def pytest_xdist_setupnodes(config: pytest.Config, specs: list[XSpec]):
 
     if config.known_args_namespace.tx[0] != 'pod':
         return
     
-    global process_list # list of subprocesses responsible for port-forwarding
+    global process_list     # list of subprocesses responsible for port-forwarding
     process_list = []
-    global ws_list      # list of streams for each pod' 
+    global ws_list          # list of streams for each pod' 
     ws_list = []
     
     # **********
     # * Set up *
     # **********
     global selected_namespace
-    list_of_test_files = config.option.file_or_dir      # List of pytest files to run provided through the terminal
-    list_of_test_files.append("src/Constants.py")       # Move Constants.py file because it's used by conftest.py in docker image
-    list_of_test_files.append("src/ms_socketserver.py") # Move server.py that will run from the pod
-    custom_image_list = config.option.custom_image.split(',')
     selected_namespace = config.option.namespace
+
+    custom_image_list = config.option.custom_image.split(',')
+    list_of_test_files = config.option.file_or_dir      # List of pytest files to run provided through the terminal
+
+    # Find the path of packages and test files
+    pkg_dir = Path(__file__).parent
+    test_dir = Path(list_of_test_files[0]).parent
+
+    # Move in all the necessary files from the package
+    list_of_test_files.append(f"{pkg_dir}/ms_socketserver.py") # Move server.py that will run from the pod
+    list_of_test_files.append(f"{pkg_dir}/conftest.py")
 
     if selected_namespace == NAMESPACE_NAME:
         selected_namespace = selected_namespace + '-' + str(uuid.uuid4())
   
     num_pods = len(specs)
-    generate_k8_pods(given_custom_images=custom_image_list, given_namespace_name=selected_namespace, num_pods=num_pods, list_filename=list_of_test_files) 
+    generate_k8_pods(
+        given_custom_images=custom_image_list, 
+        given_namespace_name=selected_namespace, 
+        num_pods=num_pods, 
+        list_filename=list_of_test_files, 
+        file_dir=test_dir
+    ) 
 
     # ***************************
     # * Communication with Pods *
@@ -82,7 +104,7 @@ def pytest_xdist_setupnodes(config: pytest.Config, specs: list[XSpec]):
         # NOTE: Running port-forward in background, give os.setsid to fully delete the thread at the end
         try:
             process = subprocess.Popen(
-                ["kubectl", "port-forward", "{}".format(k8_pod_name), "--namespace", "{}".format(selected_namespace), "{}:{}".format(available_port, assigned_port)],
+                ["kubectl", "port-forward", f"{k8_pod_name}", "--namespace", f"{selected_namespace}", f"{available_port}:{assigned_port}"],
                 start_new_session=True
             )
             process_list.append(process)
@@ -95,25 +117,29 @@ def pytest_xdist_setupnodes(config: pytest.Config, specs: list[XSpec]):
 
         # NOTE: Run the server.py from each pod to listen to localhost
         commands = [
-            "python /code/src/ms_socketserver.py :{}".format(assigned_port)
+            f"python /code/{test_dir}/ms_socketserver.py :{assigned_port}"
         ]
-        while ws.is_open():
-            ws.update(timeout=1)
-            if commands:
-                cm = commands.pop(0)
-                ws.write_stdin(cm + "\n")
-            else:
-                break
+        try:
+            while ws.is_open():
+                ws.update(timeout=1)
+                if commands:
+                    cm = commands.pop(0)
+                    ws.write_stdin(cm + "\n")
+                else:
+                    break
+        except:
+            logging.info("Issue related to stream happened.. Terminating")
+            delete_k8_deployment(selected_namespace)
+            raise Exception
 
         logging.info("Server file listening to port")
 
-        specs[idx].socket = '127.0.0.1:{}'.format(available_port)
+        specs[idx].socket = f'127.0.0.1:{available_port}'
         specs[idx].popen = False
 
         # NOTE: Bypass xdist checking whether the directory exists inside the k8 pod
-        root = Path('src')   # TODO: Change this to sync with k8_client's configmap
-        config.pluginmanager.get_plugin('dsession').nodemanager.roots.append(root)
-        config.pluginmanager.get_plugin('dsession').nodemanager._rsynced_specs.add((specs[idx], root))
+        config.pluginmanager.get_plugin('dsession').nodemanager.roots.append(test_dir)
+        config.pluginmanager.get_plugin('dsession').nodemanager._rsynced_specs.add((specs[idx], test_dir))
 
     # NOTE: Give extra time for threads to port-forward and run server.py from each pod
     logging.info('---- Give Extra Time for Threads to Run Tasks... ----')
--- src/Constants.py
+++
@@ -1,2 +0,0 @@
-CUSTOM_IMAGE_NAME = 'aidenseo1190/pytest-xdist-k8-image'
-NAMESPACE_NAME = 'pytest-ns'
